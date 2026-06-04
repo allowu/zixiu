ce0c9f3046427394e51a3f01357ebd15748ef65d
allowu <18381728829@163.com>
2026-06-04 18:54:43 +0800

    Update 石头剪刀布.js

--- 石头剪刀布.js
+++ 石头剪刀布.js
@@ -1,6 +1,9 @@
 let a='石头',b='剪刀',c='布',arr=[a,b,c],player,computer;
 computer=arr[Math.floor(Math.random()*arr.length)]
 player=prompt('请输入');
+if(player!='剪刀'&&player!='石头'&&player!='布'){
+    console.log('输入非法');
+}
 if(computer==player){
     console.log('电脑的决策为:'+computer);
     console.log('你的决策为:'+player);
